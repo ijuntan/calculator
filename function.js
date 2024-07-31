ac3fbc6212b43887a668dcd98712e0288cf77de0
ijuntan <juniortanaya@gmail.com>
2024-07-31 22:59:21 +0800

    feat: initialize basic files

---
+++ function.js
@@ -0,0 +1,20 @@
+const add = function(a, b) {
+    return a + b;
+  };
+  
+const subtract = function(a, b) {
+    return a - b;
+};
+
+const multiply = function(a, b) {
+return a * b;
+};
+
+const power = function(a, b) {
+    return a ** b;
+};
+
+const divide = function(a, b) {
+    if(b === 0) return "Error"
+    return a / b;
+};
