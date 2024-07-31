46e7d0f5fe5d11e435b481cc1a9ad762ef35f9d4
ijuntan <juniortanaya@gmail.com>
2024-08-01 01:04:19 +0800

    feat: add onclick event for numbers

--- main.js
+++ main.js
@@ -2,16 +2,31 @@ import { add, subtract, divide, multiply, power } from './calculation.js'
 
 const numberContainerDiv = document.querySelector('.number-container')
 
+const operandA = 0
+const operator = ""
+const operandB = 0
+
 const createNumberButton = () => {
     for(let i = 0; i < 10; i++) {
         let numberDiv = document.createElement('button');
         numberDiv.setAttribute('class', `number n-${i}`);
         numberDiv.textContent = i;
+
+        // Need for "." and "=" styling order
         if(i !== 0) numberDiv.style.order = 2 ;
+
         numberContainerDiv.appendChild(numberDiv);
     }
-
-    
 }
 
+const initEventListener = () => {
+    numberContainerDiv.addEventListener("click", (e) => {
+        if(e.target.classList[0] === "number") {
+            console.log(e.target.classList[1])
+        }
+    })
+}
+
+
 createNumberButton()
+initEventListener()
