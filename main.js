3cdab77791709efeef3bf11cbfac07b992b75b64
ijuntan <juniortanaya@gmail.com>
2024-08-01 02:01:34 +0800

    feat: update create button logic to include action button too and add logic to operand

--- main.js
+++ main.js
@@ -1,14 +1,40 @@
 import { add, subtract, divide, multiply, power } from './calculation.js'
 
 const numberContainerDiv = document.querySelector('.number-container')
-
+const actionContainerDiv = document.querySelector('.action-container')
 const screenDiv = document.querySelector(".screen .content")
 
-const operandA = 0
-const operator = ""
-const operandB = 0
+let operandA = 0
+let operator = ""
+let operandB = 0
 
-const createNumberButton = () => {
+const actions = [{
+    symbol: "+",
+    type: "add"
+},{
+    symbol: "-",
+    type: "subtract"
+},{
+    symbol: "x",
+    type: "multiplys"
+},{
+    symbol: "÷",
+    type: "divide"
+},{
+    symbol: "%",
+    type: "modulo"
+},{
+    symbol: "^",
+    type: "power"
+},{
+    symbol: "C",
+    type: "clear"
+},{
+    symbol: "←",
+    type: "backspace"
+}]
+
+const createButton = () => {
     for(let i = 0; i < 10; i++) {
         let numberDiv = document.createElement('button');
         numberDiv.setAttribute('class', `number n-${i}`);
@@ -19,6 +45,13 @@ const createNumberButton = () => {
 
         numberContainerDiv.appendChild(numberDiv);
     }
+
+    actions.forEach(action => {
+        let actionDiv = document.createElement('button');
+        actionDiv.setAttribute('class', `action ${action.type}`);
+        actionDiv.textContent = action.symbol;
+        actionContainerDiv.appendChild(actionDiv);
+    })
 }
 
 const initScreen = () => {
@@ -34,6 +67,10 @@ const initEventListener = () => {
     numberContainerDiv.addEventListener("click", (e) => {
         if(e.target.classList[0] === "number") {
             updateScreenUI(e.target.textContent)
+            if(operator !== "")
+                updateOperand("A")
+            else
+                updateOperand("B")
         }
     })
 }
@@ -41,16 +78,23 @@ const initEventListener = () => {
 // Update Screen UI for Number
 const updateScreenUI = (char) => {
     // special logic for "0" char
-
     if(screenDiv.textContent[0] === "0") {
         if(char === "0") return;
         screenDiv.textContent = char;
         return;
     }
 
+    // update as usual
     screenDiv.textContent += char
 }
 
-createNumberButton()
+const updateOperand = (operandType) => {
+    if(operandType === "A")
+        operandA = Number(screenDiv.textContent)
+    else if(operandType === "B")
+        operandB = Number(screenDiv.textContent)
+}
+
+createButton()
 initScreen()
 initEventListener()
