c90fe7df187932c9cbfd6d102539d554ec182748
ijuntan <juniortanaya@gmail.com>
2024-08-01 01:44:45 +0800

    feat: update screen ui when clicking numbers, add styles for text overflowing screen and add logic for "0" char

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ import { add, subtract, divide, multiply, power } from './calculation.js'
 
 const numberContainerDiv = document.querySelector('.number-container')
 
+const screenDiv = document.querySelector(".screen .content")
+
 const operandA = 0
 const operator = ""
 const operandB = 0
@@ -19,14 +21,36 @@ const createNumberButton = () => {
     }
 }
 
+const initScreen = () => {
+    screenDiv.textContent = "0"
+}
+
+// Click Number -> Clear screen and number if the previous input is not number or 0, otherwise append to the screen if not 0
+// Click . -> WIP
+// Click = -> WIP
+// Click C -> Clear screen and previous input
+// Click Actions -> save to operator
 const initEventListener = () => {
     numberContainerDiv.addEventListener("click", (e) => {
         if(e.target.classList[0] === "number") {
-            console.log(e.target.classList[1])
+            updateScreenUI(e.target.textContent)
         }
     })
 }
 
+// Update Screen UI for Number
+const updateScreenUI = (char) => {
+    // special logic for "0" char
+
+    if(screenDiv.textContent[0] === "0") {
+        if(char === "0") return;
+        screenDiv.textContent = char;
+        return;
+    }
+
+    screenDiv.textContent += char
+}
 
 createNumberButton()
+initScreen()
 initEventListener()
